4cc54f12a1adf509085103ab4fac9749b8a0707b
shrey-pansuriya <shreypansuriya11@gmail.com>
2024-04-19 19:04:23 -0400

    ss

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,17 +1,25 @@
 class CalendarsController < ApplicationController
     before_action :require_login
-
+  
     def show
-        start_date = params.fetch(:start_date, Date.today).to_date
-        # Use Current.user instead of current_user
-        @events = Current.user.events.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
+      start_date = params.fetch(:start_date, Date.today).to_date
+      @events = time_blocked_events(start_date)
     end
-
+  
+    private
+  
+    def time_blocked_events(start_date)
+      # Retrieve events for the week
+      events = Current.user.events.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
+      # Implement time-blocking logic here to generate time-blocked events
+      # For now, just return the original events
+      events
+    end
+  
     def require_login
-        unless session[:user_id] && User.exists?(session[:user_id])
-          flash[:alert] = 'Must sign in to use this feature.'
-          redirect_to sign_in_path
-        end
+      unless session[:user_id] && User.exists?(session[:user_id])
+        flash[:alert] = 'Must sign in to use this feature.'
+        redirect_to sign_in_path
+      end
     end
-
-end
+  end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,31 +1,50 @@
 class EventsController < ApplicationController
-    before_action :require_login
-  
-    def new
-      @event = Event.new
-    end
-  
-    def create
-      @event = Current.user.events.build(event_params)
-  
-      if @event.save
-        redirect_to calendar_path, notice: "Event created!"
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def event_params
-      params.require(:event).permit(:name, :start_time, :end_time, :priority)
-    end
-  
-    def require_login
-      unless session[:user_id] && User.exists?(session[:user_id])
-        flash[:alert] = 'Must sign in to use this feature.'
-        redirect_to sign_in_path
-      end
+  before_action :require_login
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Current.user.events.build(event_params)
+
+    if @event.save
+      redirect_to calendar_path, notice: "Event created!"
+    else
+      render :new
     end
   end
-  
+
+  def timeblock
+    # Group events by priority
+    grouped_events = Current.user.events.group_by(&:priority)
+
+    # Calculate the start and end times for each priority group
+    start_time = Time.current
+    end_time = Time.current
+
+    grouped_events.each do |priority, events|
+      total_hours = events.sum { |event| event.end_time - event.start_time }
+      end_time += total_hours
+      events.each do |event|
+        event.update(start_time: start_time, end_time: end_time)
+      end
+      start_time = end_time
+    end
+
+    redirect_to calendar_path, notice: "Events time-blocked!"
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :start_time, :end_time, :priority)
+  end
+
+  def require_login
+    unless session[:user_id] && User.exists?(session[:user_id])
+      flash[:alert] = 'Must sign in to use this feature.'
+      redirect_to sign_in_path
+    end
+  end
+end
