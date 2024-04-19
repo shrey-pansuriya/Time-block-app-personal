a8388c6cf49c16361b8d3f76192c9dc9445fb90a
shrey-pansuriya <shreypansuriya11@gmail.com>
2024-04-19 12:01:54 -0400

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_231852) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_19_153032) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_231852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "priority"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
